58675ad82888eb7d71f6486a574d01b223242b2f
Keita Ito <keitaitok@gmail.com>
2015-08-06 01:26:52 -0700

    add classes: MainTableViewCell, MessageTableViewController, MessageTableViewCell

---
+++ KeitaTwitter/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  KeitaTwitter
+//
+//  Created by Keita on 8/6/15.
+//  Copyright (c) 2015 Keita Ito. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
